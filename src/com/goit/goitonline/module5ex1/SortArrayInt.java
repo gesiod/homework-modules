39a2c1fb146a9fcff8734ddee93857e5d1fb4a6b
Grigogiy <g.gesiod@gmail.com>
2016-03-15 21:39:21 +0300

    Add New Metod

--- src/com/goit/goitonline/module5ex1/SortArrayInt.java
+++ src/com/goit/goitonline/module5ex1/SortArrayInt.java
@@ -4,4 +4,9 @@ package com.goit.goitonline.module5ex1;
  * Created by Grigoriy on 15.03.2016.
  */
 public class SortArrayInt {
+    public int[] SortArrayInt(int[] noSortArrayInt) {
+        int[] sortAI =  noSortArrayInt;
+
+        return sortAI;
+    }
 }
