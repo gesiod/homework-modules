d734b643a5992b4dd32696452b516c575b2345c0
Grigogiy <g.gesiod@gmail.com>
2016-03-18 01:07:03 +0300

    Add Homework Ex5 (UPD: delete SortArrayInt)

--- src/com/goit/goitonline/module4ex1/Distance2Points.java
+++ src/com/goit/goitonline/module4ex1/Distance2Points.java
@@ -1,6 +1,8 @@
 package com.goit.goitonline.module4ex1;
 
 import java.awt.geom.Point2D;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import static java.lang.StrictMath.sqrt;
 
@@ -12,7 +14,24 @@ public class Distance2Points {
         return sqrt (Math.pow(secondPoint.getX() - firstPoint.getX(), 2) + Math.pow(secondPoint.getY() - firstPoint.getY(), 2));
     }
 
-    public double calculateDistance2P (double firstPointX, double firstPointY, double secondPointX, double secondPointY) {
-        return sqrt (Math.pow(secondPointX - firstPointX, 2) + Math.pow(secondPointY - firstPointY, 2));
+    public static double calculateDistance2P (double firstPointX, double firstPointY, double secondPointX, double secondPointY) {
+
+            return sqrt(Math.pow(secondPointX - firstPointX, 2) + Math.pow(secondPointY - firstPointY, 2));
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите координаты X первой точки:");
+        int firsPointX = sc.nextInt();
+        System.out.println("Введите координаты Y первой точки:");
+        int firsPointY = sc.nextInt();
+        System.out.println("Введите координаты X второй точки:");
+        int secendPointX = sc.nextInt();
+        System.out.println("Введите координаты Y второй точки:");
+        int secendPointY = sc.nextInt();
+        System.out.println("Растояние между точками равно: " + calculateDistance2P(firsPointX, firsPointY, secendPointX, secendPointY));
     }
 }
+
---
+++ src/com/goit/goitonline/module6/Piano.java
@@ -0,0 +1,7 @@
+package com.goit.goitonline.module6;
+
+/**
+ * Created by Grigoriy on 07.03.2016.
+ */
+public class Piano extends MusicalInstrument {
+}
